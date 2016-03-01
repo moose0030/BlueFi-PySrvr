38fb4890dc9e67962683b148fa3f88d8be4c038d
matthewpreston <matthewpreston@hotmail.ca>
2016-03-01 18:12:57 -0500

    added bluetooth broadcast

--- bluetoothserver.py
+++ bluetoothserver.py
@@ -1,5 +1,15 @@
-import thread
+#import thread
 import time
+import bluetooth
+
+def broadcast():
+    addrs = {}
+    nearby = bluetooth.discover_devices(flush_cache=True)
+    for addr in nearby:
+        name = bluetooth.lookup_name(addr, 5)
+        print (addr, name)
+        addrs[addr] = name
+    return addrs   
 
 def listen():
 	while True:
@@ -8,11 +18,16 @@ def listen():
 			#if mode is 2:
 			thread.start_new_thread(printOutput,())
 		except ValueError:
-			print "Not a number"
+			print ("Not a number")
 
 def printOutput():
-	count = 0
- 	while count < 5:
-		time.sleep(10)
-		count+=1
-		print "Added via bluetooth"
+    count = 0
+    while count < 5:
+ #       time.sleep(10)
+        count+=1
+        print ("Added via bluetooth")
+
+if __name__ == "__main__":
+    addrs = broadcast()
+    for a in addrs:
+        print (a)
